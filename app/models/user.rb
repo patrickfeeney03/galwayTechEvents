56d37e81ca9da27d71e2ce47260540f1cb3c1d91
Patrick <patrickfeeneytamayo@gmail.com>
2024-12-18 20:20:05 +0000

    Handle if event has no date lol. Shouldn't happen but just in case.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
+  has_many :events, dependent: destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 
+  validates :name, presence: true
+  validates :last_name, presence: true
   validates :email_address, presence: true, uniqueness: true
   validates :password, presence: true, confirmation: true
 end
